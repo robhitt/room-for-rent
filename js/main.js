5b0eaa76fa26b8c45ba647532a2534b1a31e4f96
Rob Hitt <robhitt@gmail.com>
2017-05-14 00:43:07 -0400

    add responsive menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+var apt = document.getElementById('apt');
+apt.addEventListener("click", addApt);
+
+var building = document.getElementById('building');
+building.addEventListener('click', addBuilding);
+
+var neighborhood = document.getElementById('neighborhood'
+);
+neighborhood.addEventListener('click', addNeighborhood);
+
+building.style.display = 'none';
+neighborhood.style.display = 'none';
+
+function addApt() {
+  hideDetail();
+  apt.style.display = 'block';
+}
+
+function addBuilding() {
+  hideDetail();
+  building.style.display = 'block';
+}
+
+function addNeighborhood() {
+  hideDetail();
+  neighborhood.style.display = 'block';
+}
+
+function hideDetail() {
+  apt.style.display = 'none';
+  building.style.display = 'none';
+  neighborhood.style.display = 'none';
+}
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
